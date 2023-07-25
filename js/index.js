736583f926ce550009e24b387ad8625c06f52cca
Vadym Harmash <vadimvlad20032002@gmail.com>
2023-07-25 22:44:33 +0300

    added slider

--- js/index.js
+++ js/index.js
@@ -62,3 +62,29 @@ function renderHTML(posts) {
         }
     })
 })()
+
+function createCarousel() {
+    const carouselWrapper = document.querySelector('.clients__slider__wrapper')
+    const slides = document.querySelectorAll('.clients__slider__wrapper__item')
+    const prevButton = document.querySelector('.next-button')
+    const nextButton = document.querySelector('.prev-button')
+
+    let currentPosition = 0
+    const slideWidth = slides[0].clientWidth
+    const gap = 80
+
+    prevButton.addEventListener('click', function () {
+        if (currentPosition > 0) {
+            currentPosition -= 2;
+            carouselWrapper.style.transform = `translateX(-${(currentPosition * (slideWidth + gap))}px)`
+        }
+    })
+
+    nextButton.addEventListener('click', function () {
+        currentPosition += 2
+        if (currentPosition >= slides.length) currentPosition = 0
+        carouselWrapper.style.transform = `translateX(-${(currentPosition * (slideWidth + gap))}px)`
+    })
+}
+
+createCarousel()
