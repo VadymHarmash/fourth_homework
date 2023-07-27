6cdb082b586cf283a05aade63a5efbe961eea158
Vadym Harmash <vadimvlad20032002@gmail.com>
2023-07-27 18:44:10 +0300

    finished landing

---
+++ js/date.js
@@ -0,0 +1,5 @@
+const date = new Date()
+const months = [
+    'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'
+]
+const month = months[date.getMonth()]
--- js/index.js
+++ js/index.js
@@ -25,8 +25,7 @@ window.onscroll = function () {
 window.addEventListener('scroll', function () {
     let images = document.querySelectorAll('.article-image')
     let scrollLevel = document.body.scrollTop || document.documentElement.scrollTop
-    scrollLevel > 5200 && scrollLevel < 6000 ? images.forEach(image => image.style.opacity = 1) : images.forEach(image => image.style.opacity = 0)
-    console.log(scrollLevel)
+    scrollLevel > 5330 && scrollLevel < 6050 ? images.forEach(image => image.style.opacity = 1) : images.forEach(image => image.style.opacity = 0)
 })
 
 
@@ -123,3 +122,77 @@ function createSlider() {
 }
 
 createSlider()
+
+// Валідація форми
+
+function validateForm() {
+    const submitButton = document.querySelector('.button-input')
+    const inputs = document.querySelectorAll('.shop__form__input')
+    const nameRegex = /^[A-Z][a-zA-Z]*$/
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+    const letters = /^[a-zA-Z]+$/
+    const numbers = /^[0-9]+$/
+    const nameWarnings = document.querySelector('.shop__form__name__warnings')
+    const emailWarnings = document.querySelector('.shop__form__email__warnings')
+
+    submitButton.addEventListener('click', function () {
+        const nameWarningMessages = []
+        const emailWarningMessages = []
+
+        inputs.forEach((input) => {
+            if (input.classList.contains('name-input')) {
+                const value = input.value.trim()
+
+                if (!value) {
+                    if (input.classList.contains('firstname-input')) nameWarningMessages.push('Write your name')
+                    if (input.classList.contains('lastname-input')) nameWarningMessages.push('Write your surname')
+                } else {
+                    if (value[0].toUpperCase() !== value[0]) nameWarningMessages.push('First letter must be capitalized in name and surname')
+                    if (!letters.test(value)) nameWarningMessages.push('Use only latin letters')
+                    if (numbers.test(value)) nameWarningMessages.push('You cannot use numbers')
+                    if (input.value.includes(' ')) nameWarningMessages.push('Spaces are not allowed in name and surname')
+                }
+
+                input.classList.toggle('invalid', !nameRegex.test(value))
+            }
+
+            if (input.classList.contains('email-input')) {
+                const value = input.value.trim()
+
+                if (!value) {
+                    input.classList.add('invalid')
+                    emailWarningMessages.push('Write your email')
+                } else {
+                    if (emailRegex.test(value)) {
+                        input.classList.remove('invalid')
+                    } else {
+                        input.classList.add('invalid')
+                        emailWarningMessages.push('Incorrect email')
+                    }
+                    if (input.value.includes(' ')) emailWarningMessages.push('Spaces are not allowed in name and surname')
+                }
+            }
+        })
+
+        nameWarnings.innerHTML = nameWarningMessages.length > 0 ? `
+            <ul>${nameWarningMessages.map((warning) => `<li>${warning}</li>`).join('')}</ul>
+        ` : ''
+        
+        emailWarnings.innerHTML = emailWarningMessages.length > 0 ? `
+            <ul>${emailWarningMessages.map((warning) => `<li>${warning}</li>`).join('')}</ul>
+        ` : ''
+
+        if (nameWarningMessages.length === 0 && emailWarningMessages.length === 0) {
+            localStorage.setItem('Firstname', document.querySelector('.firstname-input').value)
+            localStorage.setItem('Lastname', document.querySelector('.lastname-input').value)
+            localStorage.setItem('Email', document.querySelector('.email-input').value)
+
+            if(document.querySelector('.firstname-input').value === 'Sigma') alert(`120% discount. Just today: ${date.getDate()}th ${month}`)
+        }
+    })
+}
+
+validateForm()
+
+const footer = document.querySelector('.footer__copyright')
+footer.innerHTML = `Copyright @ ${date.getFullYear()} Brandoxide.all right reserved.`
