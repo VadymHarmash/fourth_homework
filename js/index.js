4fef896086823bb532a7d23fd132e0983113a7d4
Vadym Harmash <vadimvlad20032002@gmail.com>
2023-07-27 19:03:31 +0300

    added smooth scrolling on anchors

--- js/index.js
+++ js/index.js
@@ -11,6 +11,21 @@ setTimeout(() => {
 
 
 // Події при скроллі
+// Плавний перехід по анкор-посиланням
+
+const smoothLinks = document.querySelectorAll('a[href^="#"]')
+for (let smoothLink of smoothLinks) {
+    smoothLink.addEventListener('click', function (e) {
+        e.preventDefault()
+        const id = smoothLink.getAttribute('href');
+
+        document.querySelector(id).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+    })
+}
+
 // Зміна прогрес-бару
 
 window.onscroll = function () {
