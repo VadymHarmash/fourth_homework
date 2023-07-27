1c8e1dd32890121c33846f55488da34122cd1735
Vadym Harmash <vadimvlad20032002@gmail.com>
2023-07-28 00:35:36 +0300

    security rules

--- js/weather.js
+++ js/weather.js
@@ -1,41 +1,40 @@
-const apiKey = 'cfd3e97e1333d6f64be43d8b9fea3fb8'
-const kyivCard = document.querySelector('.weather__kyiv')
-const odesaCard = document.querySelector('.weather__odesa')
-const kharkivCard = document.querySelector('.weather__kharkiv')
+// const kyivCard = document.querySelector('.weather__kyiv')
+// const odesaCard = document.querySelector('.weather__odesa')
+// const kharkivCard = document.querySelector('.weather__kharkiv')
 
-async function getWeatherData(city) {
-    try {
-        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},ua&APPID=${apiKey}`)
-        return await response.json()
-    } catch (error) {
-        return console.error(`Помилка при отриманні даних для міста ${city}:`, error)
-    }
-}
+// async function getWeatherData(city) {
+//     try {
+//         const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},ua&APPID=${apiKey}`)
+//         return await response.json()
+//     } catch (error) {
+//         return console.error(`Помилка при отриманні даних для міста ${city}:`, error)
+//     }
+// }
 
-const kyivWeather = getWeatherData('Kyiv')
-const odesaWeather = getWeatherData('Odesa')
-const kharkivWeather = getWeatherData('Kharkiv')
+// const kyivWeather = getWeatherData('Kyiv')
+// const odesaWeather = getWeatherData('Odesa')
+// const kharkivWeather = getWeatherData('Kharkiv')
 
-function renderWeatherHTML(data) {
-    let weather
-    if (data.weather[0]['main'] === 'Light rain' || data.weather[0].main === 'Rain') weather = 'rainy'
-    if (data.weather[0]['main'] === 'Clouds') weather = 'cloudy'
-    if (data.weather[0]['main'] === 'Sun') weather = 'sunny'
+// function renderWeatherHTML(data) {
+//     let weather
+//     if (data.weather[0]['main'] === 'Light rain' || data.weather[0].main === 'Rain') weather = 'rainy'
+//     if (data.weather[0]['main'] === 'Clouds') weather = 'cloudy'
+//     if (data.weather[0]['main'] === 'Sun') weather = 'sunny'
 
-    const source = `../../icons/${weather}.png`
+//     const source = `../../icons/${weather}.png`
 
-    return `
-        <h4>${data.name}</h4>
-        <img src='${source}'><img>
-        <p>${Math.round(data.main.temp - 273) + '&deg;'}</p>
-        <span>${data.weather[0].description}</span>
-    `
-}
+//     return `
+//         <h4>${data.name}</h4>
+//         <img src='${source}'><img>
+//         <p>${Math.round(data.main.temp - 273) + '&deg;'}</p>
+//         <span>${data.weather[0].description}</span>
+//     `
+// }
 
-Promise.all([kyivWeather, odesaWeather, kharkivWeather])
-    .then(data => {
-        const [kyivData, odesaData, kharkivData] = data
-        odesaCard.innerHTML = renderWeatherHTML(odesaData)
-        kyivCard.innerHTML = renderWeatherHTML(kyivData)
-        kharkivCard.innerHTML = renderWeatherHTML(kharkivData)
-    })
+// Promise.all([kyivWeather, odesaWeather, kharkivWeather])
+//     .then(data => {
+//         const [kyivData, odesaData, kharkivData] = data
+//         odesaCard.innerHTML = renderWeatherHTML(odesaData)
+//         kyivCard.innerHTML = renderWeatherHTML(kyivData)
+//         kharkivCard.innerHTML = renderWeatherHTML(kharkivData)
+//     })
